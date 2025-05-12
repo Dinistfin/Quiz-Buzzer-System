fab2f1930466a3de771fdb2288bbbf4ce83e23ab
bpmryandinh <bdinh@my.okcu.edu>
2025-05-12 18:59:45 -0500

    added arduino file

---
+++ sketch_mar28a_jeopardy/sketch_mar28a_jeopardy.ino
@@ -0,0 +1,57 @@
+#include "stm32f1xx_hal.h"
+
+
+//this is a test commit comment 
+//does this work???????? 
+
+typedef enum {
+  idle
+} Mode;
+
+volatile Mode mode = idle;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(PC15,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(PC15,HIGH);
+  delay(1000);
+  digitalWrite(PC15,LOW);
+  delay(500);
+
+  switch (mode){
+    default: //idle mode
+    // statements 
+    // Question is being read
+    // All resets lead to this case
+    
+    break;
+
+    case 0: //false start
+    // statements
+    // Happens when player presses too early
+    // Should light up any teams that pressed early
+
+    break;
+    
+    case 1: //watch
+    // statements
+    // watches for players for buzz in
+
+    break;
+
+    case 2: //capture
+    // statements
+    //1st player to buzz in has a solid led
+    //2nd pllayer to buzz in had a blinking led
+
+    break;
+  }
+}
+   
+
+
